1d6557a531068ac5c722ff2c929204fc79339041
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-17 02:59:25 -0700

    Add project files.

---
+++ QuickShort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QuickShort
+{
+    class Program
+    {
+        //array of integers to hold valuea
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        //number of elements in array
+        private int n;
+
+        void read ()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.pasrse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+
+            }
+            Console
+        }
+    }
+}
