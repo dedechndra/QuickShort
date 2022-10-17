e29af63766084e8c158797b30634a673211d805c
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-17 03:30:59 -0700

    search for an element less than equal to pivot

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -71,10 +71,20 @@ namespace QuickShort
                 }
                 cmp_count++;
 
+                //search for an element less than equal to pivot
+                while
+                     ((arr[j] > pivot) && (j >= low))
+                     {
+                        j--;
+                    cmp_count++;
+                     }
+                cmp_count++;
+
                 if (i < j) //if the greater element is on the left of the element
                 {
                     //swap the element at index i whit the element at index j
                     swap(i, j);
+
 
                 }
             }
