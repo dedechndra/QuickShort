fbd70ff23708d9d2b8c782fa4ffa624f7de68134
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-17 03:05:54 -0700

    get array elements

--- QuickShort/Program.cs
+++ QuickShort/Program.cs
@@ -18,14 +18,24 @@ namespace QuickShort
             {
                 Console.Write("Enter the number of elements in the array :");
                 string s = Console.ReadLine();
-                n = Int32.pasrse(s);
+                n = Int32.Parse(s);
                 if (n <= 20)
                     break;
                 else
                     Console.WriteLine("\nArray can have maximum 20 elements \n");
 
             }
-            Console
+            Console.WriteLine("\n=======================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n=======================");
+
+            //get array elements 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
     }
 }
